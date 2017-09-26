1fe1e7b152b4f8236c08c2109ca7d26e78834904
Félix Prado <mail@felixprado.com>
2017-09-26 12:20:52 +0200

    Added last classes and tests of the game

--- src/main/kotlin/bj1/Hand.kt
+++ src/main/kotlin/bj1/Hand.kt
@@ -1,2 +1,31 @@
-package bj1 
+package bj1
 
+class Hand(val name: String) {
+
+    private val _cards = mutableListOf<Card>()
+
+    val cards: List<Card> get() = _cards
+
+    fun add(card: Card) {
+        _cards.add(card)
+    }
+
+    val size get() = _cards.size
+
+    val points1: Int get () {
+        var t = 0
+        for (c in _cards) t += c.points
+        return t
+    }
+
+
+    val points2: Int get () {
+        var t = 0
+        _cards.forEach({ t += it.points })
+        return t
+    }
+
+
+    val points get () =  _cards.sumBy { it.points }
+
+}
--- src/test/kotlin/bj1/HandTest.kt
+++ src/test/kotlin/bj1/HandTest.kt
@@ -1,4 +1,48 @@
 package bj1
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class HandTest {
+
+    @Test
+    fun t1() {
+        val h1 = Hand("Player");
+        h1.add(Card(1,1))
+        h1.add(Card(13,4))
+
+        val h2 = Hand("Dealer");
+        h2.add(Card(1,1))
+        h2.add(Card(2,1))
+        h2.add(Card(3,1))
+
+        assertEquals("Player", h1.name)
+        assertEquals(11, h1.points)
+        assertEquals(2, h1.size)
+
+        assertEquals("Dealer", h2.name)
+        assertEquals(6, h2.points)
+        assertEquals(3, h2.size);
+
+        renderHand(h1)
+        println()
+        renderHand(h2)
+    }
+
+    fun renderHand(h: Hand){
+        println("${h.name} Hand")
+        h.cards.forEach {
+            println(it.name)
+        }
+        println("${h.points} points")
+    }
+
+
+    @Test
+    fun t2() {
+
+
+    }
+
 }
+
